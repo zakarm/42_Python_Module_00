b5a4c6ff84ad7df81804dc5947bb98291baed637
zaka_rm <zakariaemrabet1@gmail.com>
2022-12-29 19:27:05 +0100

    first commit

--- EX01/exec.py
+++ EX01/exec.py
@@ -1,13 +1,34 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    exec.py                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: zmrabet <zmrabet>                          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/12/29 19:17:39 by zmrabet           #+#    #+#              #
+#    Updated: 2022/12/29 19:17:39 by zmrabet          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 import sys
 
 def reverse_strings(string) :
+    data = ""
     len_s = len(string)
-    while (len_s >= 0):
-        print(string[len_s])
+    while (len_s > 0):
+        data += string[len_s - 1]
         len_s = len_s - 1
+    print(data)
 
 def main() :
-    reverse_strings(sys.argv[0])
+    i = 1
+    concat = ""
+    if (len(sys.argv) >= 2) :
+        while (i < len(sys.argv)):
+            concat += sys.argv[i]
+            concat +=  " "
+            i = i + 1;
+        reverse_strings(concat)
 
 if __name__ == "__main__":
     main()
