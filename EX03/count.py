a03da5e037e1457196b2c2e9caea54c22673ff60
zaka_rm <zakariaemrabet1@gmail.com>
2022-12-30 01:05:18 +0100

    commit 1

--- EX03/count.py
+++ EX03/count.py
@@ -1,16 +1,23 @@
 import sys
 import string 
 
-def text_analyzer(string) :
+def punc_c(string_c):
+    count = 0
+    for i in string_c :
+        if (i in string.punctuation):
+            count += 1
+    return count
+
+def text_analyzer(string_c) :
     upper = punc = lower = space = 0
-    for i in string :
-        if (i.isupper):
+    for i in string_c :
+        if (i.isupper()):
             upper += 1
-        elif (i.islower):
+        elif (i.islower()):
             lower += 1
-        elif (i.isspace):
+        elif (i.isspace()):
             space += 1
-    punc = 14
+    punc = punc_c(string_c)
     print("- " + str(upper) + " upper letter(s)")
     print("- " + str(lower) + " lower letter(s)")
     print("- " + str(punc) + " punctuation mark(s)")
