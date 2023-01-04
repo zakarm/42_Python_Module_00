055f9a74653aa4b4854c6728ce53b13ba1605b82
Zakariae Mrabet <82889188+zakarm@users.noreply.github.com>
2023-01-04 15:55:04 +0100

    commit

--- EX05/Kata01.py
+++ EX05/Kata01.py
@@ -1,29 +1,19 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    Kata00.py                                          :+:      :+:    :+:    #
+#    Kata01.py                                          :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: zmrabet <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2022/12/30 11:59:37 by zmrabet           #+#    #+#              #
-#    Updated: 2023/01/04 15:54:49 by zmrabet          ###   ########.fr        #
+#    Created: 2023/01/04 15:36:18 by zmrabet           #+#    #+#              #
+#    Updated: 2023/01/04 15:38:07 by zmrabet          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-import sys
+import sys 
 
 def main():
-    k = 0
-    kata = (19,42,21)
-    len_t = len(kata);
-    data = ""
-    sys.stdout.write("The " + str(len_t) + " numbers are: ")
-    for i in kata :
-        data += str(i)
-        if (k != len_t - 1):
-            data += ","
-        k += 1
-    print("".join(str(data)));
+    print("Hello")
 
-if __name__ == "__main__":
+if __name__ == "__main__" :
     main()
